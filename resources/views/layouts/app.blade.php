580a72dea62e3c319ff1f126929570ee7a59e5bf
vagrant <vagrant@homestead>
2019-08-27 02:18:29 +0000

    手机禁止缩放

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;"/>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <title>@yield('title', 'BULL') - 上海公牛</title>
