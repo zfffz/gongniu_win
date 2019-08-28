8eb5ae2cfbb3d8e80ca9cfe5e9259d8ab15bb8c6
vagrant <vagrant@homestead>
2019-08-28 10:01:49 +0000

    样式调整

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <a href="#" class="navbar-brand">
             <img src="/AdminLTE/dist/img/logo.png" alt="AdminLTE Logo" class="brand-image"
-                 style="opacity: .8">
+                 style="opacity: .8;margin-left:0px;margin-right:0px;">
         </a>
 
         <ul class="navbar-nav">
