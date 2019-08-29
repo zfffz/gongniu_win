ca5a849b4c39d18631e9a5bf6f23de740809a188
vagrant <vagrant@homestead>
2019-08-29 05:09:04 +0000

    打包出库页面编辑

--- resources/views/sweepOuts/create.blade.php
+++ resources/views/sweepOuts/create.blade.php
@@ -211,7 +211,23 @@
                 //如果不等于，弹窗提示
                 if(location_no.length >=4){
                     if(location_no != $('#location_no_default').val()){
-                        alert(1);
+                        Swal.fire({
+                            title: 'Are you sure?',
+                            text: "You won't be able to revert this!",
+                            type: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Yes, delete it!'
+                        }).then((result) => {
+                            if (result.value) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    });
                     }else{
                         addRow('text-success');
                     }
