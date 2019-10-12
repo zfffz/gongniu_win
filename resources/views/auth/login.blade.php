a17e77f321346709db920384dd788f0f0b641ed8
vagrant <vagrant@homestead>
2019-10-12 09:32:55 +0000

    login界面

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
 <div id="app" class="{{ route_class() }}-page"></div>
 <div class="login-box">
     <div class="login-logo">
-        <img src="/image/logo.png" alt="AdminLTE Logo" class="brand-image" style="height:36px;">
+        <img src="/image/logo.png" alt="AdminLTE Logo" class="brand-image" style="opacity: .8;height:34px;">
     </div>
 
     <!-- /.login-logo -->
