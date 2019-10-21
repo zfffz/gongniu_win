642cacee82c28723d0768f8540909d559a1cac0a
vagrant <vagrant@homestead>
2019-10-21 07:16:48 +0000

    sweetalert2

--- app/Http/Controllers/SweepCarsController.php
+++ app/Http/Controllers/SweepCarsController.php
@@ -138,7 +138,7 @@ class SweepCarsController extends CommonsController
     public function checkPass(Request $request){
         $password = $request->password;
         if($password == '123456'){
-            echo '{"status":"success"}';
+            echo json_encode(array('status'=>'success'));
         }else{
             echo json_encode(array('status'=>'error'));
         }
